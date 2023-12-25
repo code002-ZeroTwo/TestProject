92fb6496a8a91680a36fdbb5c9da825364b3222d
siddhartha <siddharthpaudel41@gmail.com>
2023-12-25 18:46:02 +0545

    worked with frontend

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,138 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import "./Home.css";
+
+const Home = (props) => {
+    const [content, setContent] = useState({});
+    const token = localStorage.getItem("access_token");
+
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("http://localhost:8000/api/user", {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                credentials: "include",
+            });
+
+            const content = await response.json();
+            if (response.ok) {
+                setContent(content.data);
+                console.log(content.data);
+                props.setName(content.data.name);
+                props.setLoggedin(true);
+            } else {
+                // Handle error...
+                console.error("Fetch failed:", response.statusText);
+            }
+
+            console.log(content);
+
+        })();
+    }, []);
+
+    if (!props.loggedin) {
+        console.log("is this the one in wrong")
+        return <Navigate to="/login" />;
+    }
+
+    return (
+        <div className="card-container">
+            <div className="card">
+                <div className="upper-section">
+                    <div>
+                        <div></div>
+
+                        <div className="text-container">Identity Card</div>
+                        <div className="text-container">
+                            Identity Card Type: 'D'
+                        </div>
+                    </div>
+                    <div className="texts">
+                        <div className="Red-text-container">
+                            Palungtar Municipality
+                        </div>
+                        <div className="Red-Big-text-container">
+                            Office Of The Municipal Executive
+                        </div>
+                        <div className="Big-text-container">Palungtar,Gorkha</div>
+                        <div className="Big-text-container">
+                            Gandaki Province,Nepal
+                        </div>
+
+                        <div className="DisabilityIDCard">
+                            Disability Identity Card
+                        </div>
+                    </div>
+                </div>
+
+                <div className="middle-section text-container">
+                    <div className="address-details">
+                        <p>Full Name of person: {content.name}</p>
+                    </div>
+                    <div className="address-details">
+                        <p>
+                            <strong>Address: Gandaki province</strong>
+                            <strong>District: Gorkha</strong>
+                            <strong>Palungtar Municipality</strong>
+                            <strong>Ward No:{content.wardno}</strong>
+                        </p>
+                    </div>
+
+                    <div className="address-details">
+                        <p>
+                            <strong>Date of Birth: {content.dateofbirth}</strong>
+                            <strong>Citizenship Number:{content.citizenshipno}</strong>
+                            <strong>Sex:{content.sex} </strong>
+                            <strong>Blood Group:{content.bloodgroup}</strong>
+                        </p>
+                    </div>
+
+                    <div className="address-details">
+                        <p>
+                            <strong>Types of disability:</strong>
+                            <strong>On the basis of nature: {content.nature}</strong>
+                            <strong>On the basis of Severity:{content.severity} </strong>
+                        </p>
+                    </div>
+
+                    <div className="address-details">
+                        <p>
+                            <strong>
+                                Father Name or Mother Name or Guardian Name: {content.guardianname}
+                            </strong>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="lower-section">
+                    <div className="left-side">
+                        <div>Approved by</div>
+                        <p>Name: </p>
+                        <p>Signature: </p>
+                        <p>Date: </p>
+                    </div>
+
+                    <div className="right-side">
+                        <p>...............................................</p>
+                        <p className="text">Signature of ID card Holder</p>
+                    </div>
+                </div>
+
+                <div className="lowest-section">
+                    <p>
+                        "if somebody finds this ID card, please deposit this in
+                        the nearby police or municipality office"
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,175 @@
+import React from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+const Register = (props) => {
+    const [name, setName] = useState("");
+    const [ward, setWardNo] = useState("");
+    const [dob, setDate] = useState("");
+    const [citizenship, setCitizenship] = useState("");
+    const [sex, setSex] = useState("");
+    const [bloodgroup, setBloodGroup] = useState("");
+    const [nature, setNature] = useState("");
+    const [severity, setSeverity] = useState("");
+    const [guardianname, setGuardian] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [navigate, setNavigate] = useState(false);
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        try{
+        const response = await fetch("http://localhost:8000/api/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                wardno: ward,
+                citizenshipno: citizenship,
+                sex: sex,
+                bloodgroup: bloodgroup,
+                dateofbirth: dob,
+                nature: nature,
+                severity: severity,
+                guardianname: guardianname,
+                password: password,
+            }),
+        });
+
+        if (response.status === 200) {
+            setNavigate(true);
+            console.log("registration  success");
+            props.setLoggedin(true);
+        } else {
+            console.log("registration failed");
+            throw new Error(`error ${response.status}`);
+        }
+        const token = await response.json();
+        props.setToken(token.access_token);
+        console.log(token.access_token);
+        localStorage.setItem("access_token", token.access_token);
+
+    }
+    catch(error){
+        console.log(error);
+    }
+
+    };
+    if (props.loggedin) {
+        return <Navigate to="/" />;
+    }
+
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Full Name"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="number"
+                        className="form-control"
+                        placeholder="ward number"
+                        onChange={(e) => setWardNo(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="date"
+                        name="dob"
+                        className="form-control"
+                        placeholder="date of birth"
+                        onChange={(e) => setDate(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="citizenship number"
+                        onChange={(e) => setCitizenship(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="sex"
+                        onChange={(e) => setSex(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="bloodgroup"
+                        onChange={(e) => setBloodGroup(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Disability: Nature"
+                        onChange={(e) => setNature(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Disability: Severity"
+                        onChange={(e) => setSeverity(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Guardian Name"
+                        onChange={(e) => setGuardian(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="name@example.com"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <br />
+
+                <button className="btn btn-primary w-100 py-2" type="submit">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
